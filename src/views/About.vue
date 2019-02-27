762cbd502cdf747c43a890ffcc6ea4202aa193d1
陈其文 <jianwang19@sina.com>
2019-02-27 17:42:45 +0800

    实验性尝试

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="wrap-swiper">
+      <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+        <!-- slides -->
+        <swiper-slide><img src="@/assets/logo.png" alt=""></swiper-slide>
+        <swiper-slide><img src="@/assets/logo.png" alt=""></swiper-slide>
+        <swiper-slide><img src="@/assets/logo.png" alt=""></swiper-slide>
+        <swiper-slide><img src="@/assets/logo.png" alt=""></swiper-slide>
+        <swiper-slide><img src="@/assets/logo.png" alt=""></swiper-slide>
+        <swiper-slide><img src="@/assets/logo.png" alt=""></swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+@Component({
+  components: {
+    swiper,
+    swiperSlide
+  }
+})
+export default class About extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+.wrap-swiper,.swiper-container {
+  height: 200px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" names="cqw" />
   </div>
 </template>
 
