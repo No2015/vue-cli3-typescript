984688f855dd00b70da0e0e966c41ceb84311de1
陈其文 <jianwang19@sina.com>
2019-03-11 11:15:26 +0800

    购物车

--- src/components/cart/Index.vue
+++ src/components/cart/Index.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="content">
     <div class="list">
-      <div v-for="(item, index) in cart" :key="index" class="clearfix item">
-        <div class="pull-left item-left">
-          <span class="hand" @click="toUrl(item.id)">
-            <img :src="item.img" :alt="item.id">
-          </span>
+      <div v-for="(cate, index) in cart" :key="index" class="cate clearfix">
+        <div class="item-title">
+          <ul class="cate-head flex-box">
+            <li class="check-left">
+              <span :class="'icon-check'" @click="checkCate(cate)"></span>
+            </li>
+            <li class="check-right">
+              <span class="cate-title">{{ cate[0].ctitle }}</span>
+            </li>
+          </ul>
         </div>
-        <div class="pull-right item-right">
-          <p class="title hand" @click="toUrl(item.id)">{{ item.title }}</p>
-          <p class="price">￥{{ item.price }}</p>
-          <p class="num">{{ item.num }}</p>
+        <div v-for="(item, index) in cate" :key="index" class="item">
+          <div class="solt"></div>
+          <ul class="flex-box">
+            <li class="check-left">
+              <span :class="'icon-check'" @click="checkIndex(item, index)"></span>
+            </li>
+            <li class="check-right">
+              <div class="solt"></div>
+              <ul class="flex-box">
+                <li class="item-left">
+                  <span class="hand" @click="toUrl(item.id)">
+                    <img :src="item.img" :alt="item.id">
+                  </span>
+                </li>
+                <li class="item-right">
+                  <p class="title hand" @click="toUrl(item.id)">{{ item.title }}</p>
+                  <p class="price">￥{{ item.price }}</p>
+                  <p class="num">{{ item.num }}</p>
+                </li>
+              </ul>
+              <div class="solt"></div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -23,10 +47,27 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class CartIndex extends Vue {
   get cart() {
-    return this.$store.state.cartList;
+    const list = this.$store.state.cartList;
+    let cart = {};
+    for(let i = 0; i < list.length; i++) {
+      let cate = list[i].cate;
+      if (cart[cate]) {
+        cart[cate].push(list[i]);
+      }else{
+        cart[cate] = [];
+        cart[cate].push(list[i]);
+      }
+    }
+    return cart;
   }
   private toUrl(gid: string) {
-    this.$router.push({ path: 'goods', query: { gid: gid } });
+    this.$router.push({ path: 'goods', query: { gid } });
+  }
+  private checkCate(cate: any) {
+    console.log(cate);
+  }
+  private checkIndex(item: any, index: number) {
+    console.log(item, index);
   }
 }
 </script>
@@ -40,24 +81,54 @@ export default class CartIndex extends Vue {
   width: 100%;
   z-index: 1;
   overflow: auto;
-  padding: 10px;
   .list{
-    padding-bottom: 10px;
-    .item{
-      padding: 10px 0;
-      border-bottom: 1px solid #f3f3f3;
-      .item-left{
-        width: 80px;
-        height: 80px;
-        img{
-          width: 80px;
-          height: 80px;
+    padding: 10px;
+    .cate{
+      padding-bottom: 10px;
+      .cate-head{
+        padding: 10px 0;
+        .cate-title{
+          display: inline-block;
+          border-left: 2px solid #ff5a0c;
+          padding-left: 5px;
+          height: 18px;
+          line-height: 20px;
+          font-size: 16px;
+          color: #333;
         }
       }
-      .item-right{
-        width: calc(100% - 90px);
-        height: 80px;
+      .item{
+        padding: 10px 0;
+        border-bottom: 1px solid #f3f3f3;
+        .item-left{
+          width: 80px;
+          height: 80px;
+          img{
+            width: 80px;
+            height: 80px;
+          }
+        }
+        .item-right{
+          width: calc(100% - 90px);
+          height: 80px;
+          padding-left: 10px;
+          .title{
+            word-wrap: break-word;
+          }
+        }
       }
+      .check-left{
+        width: 30px;
+      }
+      .check-right{
+        width: calc(100% - 30px);
+      }
+    }
+    .icon-check{
+      height: 20px;
+      display: block;
+      width: 20px;
+      cursor: pointer;
     }
   }
 }
