2c5f4fda186756aa924d8ec0491420d47cce8a48
陈其文 <jianwang19@sina.com>
2019-06-18 14:57:08 +0800

    vue-router 版本

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@ import { Component, Mixins } from 'vue-property-decorator';
 import Header from '@/components/layout/Header.vue';
 import Content from '@/components/home/Index.vue';
 import Footer from '@/components/layout/Footer.vue';
-import { Route } from '_vue-router@3.0.2@vue-router';
+import { Route } from 'vue-router';
 import MyMixin from '@/global/mixin';
 
 @Component({
